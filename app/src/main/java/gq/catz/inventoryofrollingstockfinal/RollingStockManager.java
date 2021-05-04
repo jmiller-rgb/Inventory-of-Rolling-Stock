0b129a25f3f68b6cb7bcf8ca3f3566cb69e4df4a
cschlack <ctschlack02@gmail.com>
2021-05-04 08:42:15 -0400

    Added ctor and getInstance to RollingStockManager class.

--- app/src/main/java/gq/catz/inventoryofrollingstockfinal/RollingStockManager.java
+++ app/src/main/java/gq/catz/inventoryofrollingstockfinal/RollingStockManager.java
@@ -1,5 +1,17 @@
 package gq.catz.inventoryofrollingstockfinal;
 
+import android.content.Context;
+
 public class RollingStockManager {
+    private Context context;
+
+    public static RollingStockManager getInstance(Context context){
+        return new RollingStockManager(context);
+
+    }
+
+    private RollingStockManager(Context context) {
+        this.context = context;
+    }
 
 }
